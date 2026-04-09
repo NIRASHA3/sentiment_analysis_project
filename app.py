d5c68000e253c308fd8f917d4548d055ebaf4181
NIRASHA3 <nirashadeshani3@gmail.com>
2026-04-09 14:54:23 +0530

    created a pipeline and build a web app

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template,request, redirect
+from helper import preprocessing, vectorizer, get_prediction
+from logger import logging
+
+app = Flask(__name__)
+
+logging.info('Flask server started')
+
+data = {}
+reviews = []
+positive = 0
+negative = 0
+
+@app.route("/", methods=['GET'])
+def index():
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+
+    logging.info('========== Open home page ============')
+
+    return render_template('index.html', data=data)
+
+@app.route("/", methods=['POST'])
+def my_post():
+    text = request.form['text']
+    logging.info(f'Text : {text}')
+
+    preprocessed_txt = preprocessing(text)
+    logging.info(f'Preprocessed Text : {preprocessed_txt}')
+
+    vectorized_txt = vectorizer(preprocessed_txt)
+    logging.info(f'Vectorized Text : {vectorized_txt}')
+
+    prediction = get_prediction(vectorized_txt)
+    logging.info(f'Prediction : {prediction}')
+
+    if prediction == 'negative':
+        global negative
+        negative += 1
+        sentiment = 'negative'
+    else:
+        global positive
+        positive += 1
+        sentiment = 'positive'
+
+    reviews.insert(0, {
+        'text': text,
+        'sentiment': sentiment,
+    })
+
+    return redirect(request.url)
+
+if __name__ == "__main__":
+    app.run()
